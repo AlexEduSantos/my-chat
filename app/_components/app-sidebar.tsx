be41a9a5b5f1c1c1f77b161eba1d6ab28c6f7eff
Alex Edurdo <125306500+TheDuxX@users.noreply.github.com>
2025-11-25 21:16:01 -0300

    feat: add shadcn dropdown menu

--- app/_components/app-sidebar.tsx
+++ app/_components/app-sidebar.tsx
@@ -10,8 +10,14 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "./ui/sidebar";
-import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { EllipsisVerticalIcon, LogOutIcon, SettingsIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const AppSidebar = () => {
   const { user, isLoading } = useUser();
@@ -26,7 +32,7 @@ const AppSidebar = () => {
       <Sidebar className="">
         <SidebarHeader className="border-b border-sidebar-border">
           <SidebarMenu>
-            <SidebarMenuItem className="flex gap-2 items-center p-2">
+            <SidebarMenuItem className="flex justify-between items-center p-2">
               <div className="flex gap-2 items-center">
                 {user.avatar?.length ? (
                   <div className="relative w-10 h-10">
@@ -46,14 +52,23 @@ const AppSidebar = () => {
                   <p className="text-sm font-light">{user.email}</p>
                 </div>
               </div>
-              <div className="flex gap-2">
-                <Button size="icon-sm" className="" variant="secondary">
-                  <SettingsIcon className="w-4 h-4" />
-                </Button>
-                <Button size="icon-sm" variant="secondary">
-                  <LogOutIcon className="w-4 h-4" />
-                </Button>
-              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="secondary" size="icon">
+                    <EllipsisVerticalIcon className="w-4 h-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                  <DropdownMenuItem>
+                    <SettingsIcon className="w-4 h-4 mr-2" />
+                    <p>Settings</p>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <LogOutIcon className="w-4 h-4 mr-2" />
+                    <p>Logout</p>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </SidebarMenuItem>
           </SidebarMenu>
         </SidebarHeader>
